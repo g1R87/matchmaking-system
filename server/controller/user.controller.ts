58b0df382b0f9bbc7992cc8c6b453a995c3ae362
g1R87 <giri.sujan87@gmail.com>
2023-04-12 12:47:18 +0545

    login

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -1,15 +1,42 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import BadRequestError from "../errors/bad-request";
+import UnauthenticatedError from "../errors/unauthenticated";
+
 import User from "../models/user.model";
+
+export const login = async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Email/Password is required");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials / User doesnt exist");
+  }
+
+  const isPasswordCorrect = await user.checkPassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user, token });
+};
 
 export const signup = async (req: Request, res: Response) => {
   console.log(req.body);
 
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
-  res.status(201).send({ user, token });
+  res.status(StatusCodes.CREATED).send({ user, token });
 };
 
+//temporary
 export const getUser = async (req: Request, res: Response) => {
   const users = await User.find();
-  res.status(200).send(users);
+  res.status(StatusCodes.OK).send(users);
 };
--- server/router/user.router.ts
+++ server/router/user.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import * as userController from "../controller/user.controller";
 const userRouter = Router();
 
+userRouter.post("/login", userController.login);
 userRouter.post("/signup", userController.signup);
 userRouter.get("/users", userController.getUser);
 
